7569422badc4fe3a43cb99c903056daba0a4439b
Edward Garmon <garmonedward@gmail.com>
2022-07-11 16:02:41 -0500

    initial game simulation

---
+++ game_actions.go
@@ -0,0 +1,166 @@
+package main
+
+import "github.com/BattlesnakeOfficial/rules"
+
+type Direction string
+
+type Simulation struct {
+	board     rules.BoardState
+	settings  rules.Settings
+	rules_set rules.StandardRuleset
+}
+
+type Tree struct {
+	settings  rules.Settings
+	rules_set rules.StandardRuleset
+}
+
+// func (game *Simulation) getAllValidMoves() [][]rules.SnakeMove {
+// }
+
+func simulationFromGame(game *GameState) Simulation {
+	return Simulation{
+		board: rules.BoardState{
+			Height: game.Board.Height,
+			Width:  game.Board.Width,
+			Snakes: convertSnakes(game.Board.Snakes),
+		},
+	}
+}
+
+func convertSnakes(api_snakes []Battlesnake) []rules.Snake {
+	var real_snakes = []rules.Snake{}
+	for _, snake := range api_snakes {
+		real_snakes = append(real_snakes,
+			rules.Snake{
+				ID:               snake.ID,
+				Body:             convertBody(snake.Body),
+				Health:           int(snake.Health),
+				EliminatedCause:  rules.NotEliminated,
+				EliminatedOnTurn: 0,
+				EliminatedBy:     rules.NotEliminated,
+			})
+	}
+	return real_snakes
+}
+
+func convertBody(body []Coord) []rules.Point {
+	var new_body = []rules.Point{}
+	for _, bod := range body {
+		new_body = append(new_body, rules.Point{
+			X: bod.X,
+			Y: bod.Y,
+		})
+	}
+	return new_body
+}
+
+func (game *Simulation) getValidMoves(snakeId string) []rules.SnakeMove {
+
+	snake := get_snake(game.board, snakeId)
+
+	var dirs = [][]int{{1, 0}, {-1, 0}, {0, -1}, {0, 1}}
+
+	var valid_moves = []rules.SnakeMove{}
+
+	for _, dir := range dirs {
+
+		x_dir := dir[0]
+		y_dir := dir[1]
+
+		head := snake.Body[0]
+
+		new_head := rules.Point{}
+		new_head.X = head.X + x_dir
+		new_head.Y = head.Y + y_dir
+
+		//check for neck collision
+		if len(snake.Body) > 1 {
+			neck := snake.Body[1]
+			if neck.X == new_head.X && neck.Y == new_head.Y {
+				continue
+			}
+		}
+
+		// check for wall collisions
+
+		if new_head.X >= game.board.Width || new_head.X < 0 || new_head.Y >= game.board.Height || new_head.Y < 0 {
+			continue
+		}
+
+		valid_move := true
+
+		// check for snake collisions
+
+		for _, other_snake := range game.board.Snakes {
+
+			if snakeHasLostHeadToHead(snake, &other_snake) {
+				valid_move = false
+				break
+			}
+
+			if snakeHasBodyCollided(snake, &other_snake) {
+				valid_move = false
+				break
+			}
+
+		}
+
+		if valid_move {
+			valid_moves = append(valid_moves, rules.SnakeMove{
+				ID:   snakeId,
+				Move: getMoveFromDir(dir),
+			})
+		}
+	}
+
+	return valid_moves
+}
+
+func getMoveFromDir(dir []int) string {
+	if dir[0] == 1 {
+		return rules.MoveRight
+	}
+	if dir[0] == -1 {
+		return rules.MoveLeft
+	}
+
+	if dir[1] == 1 {
+		return rules.MoveUp
+	} else {
+
+		return rules.MoveDown
+	}
+}
+
+func snakeHasLostHeadToHead(s *rules.Snake, other *rules.Snake) bool {
+	if s.Body[0].X == other.Body[0].X && s.Body[0].Y == other.Body[0].Y {
+		return len(s.Body) <= len(other.Body)
+	}
+	return false
+}
+
+func snakeHasBodyCollided(s *rules.Snake, other *rules.Snake) bool {
+	head := s.Body[0]
+	for i, body := range other.Body {
+		if i == 0 {
+			continue
+		} else if head.X == body.X && head.Y == body.Y {
+			return true
+		}
+	}
+	return false
+}
+
+func get_snake(board rules.BoardState, snakeId string) *rules.Snake {
+	for _, snake := range board.Snakes {
+		if snake.ID == snakeId {
+			return &snake
+		}
+	}
+	return nil
+}
+
+func (game *Simulation) executeActions(moves []rules.SnakeMove) (bool, *rules.BoardState, error) {
+	return game.rules_set.Execute(&game.board, game.settings, moves)
+}
--- logic.go
+++ logic.go
@@ -7,7 +7,8 @@ package main
 
 import (
 	"log"
-	"math/rand"
+
+	"github.com/BattlesnakeOfficial/rules"
 )
 
 // This function is called when you register your Battlesnake on play.battlesnake.com
@@ -43,60 +44,18 @@ func end(state GameState) {
 // where to move -- valid moves are "up", "down", "left", or "right".
 // We've provided some code and comments to get you started.
 func move(state GameState) BattlesnakeMoveResponse {
-	possibleMoves := map[string]bool{
-		"up":    true,
-		"down":  true,
-		"left":  true,
-		"right": true,
-	}
 
-	// Step 0: Don't let your Battlesnake move back in on it's own neck
-	myHead := state.You.Body[0] // Coordinates of your head
-	myNeck := state.You.Body[1] // Coordinates of body piece directly behind your head (your "neck")
-	if myNeck.X < myHead.X {
-		possibleMoves["left"] = false
-	} else if myNeck.X > myHead.X {
-		possibleMoves["right"] = false
-	} else if myNeck.Y < myHead.Y {
-		possibleMoves["down"] = false
-	} else if myNeck.Y > myHead.Y {
-		possibleMoves["up"] = false
-	}
+	sim := simulationFromGame(&state)
 
-	// TODO: Step 1 - Don't hit walls.
-	// Use information in GameState to prevent your Battlesnake from moving beyond the boundaries of the board.
-	// boardWidth := state.Board.Width
-	// boardHeight := state.Board.Height
+	valid_moves := sim.getValidMoves(state.You.ID)
 
-	// TODO: Step 2 - Don't hit yourself.
-	// Use information in GameState to prevent your Battlesnake from colliding with itself.
-	// mybody := state.You.Body
-
-	// TODO: Step 3 - Don't collide with others.
-	// Use information in GameState to prevent your Battlesnake from colliding with others.
-
-	// TODO: Step 4 - Find food.
-	// Use information in GameState to seek out and find food.
-
-	// Finally, choose a move from the available safe moves.
-	// TODO: Step 5 - Select a move to make based on strategy, rather than random.
-	var nextMove string
-
-	safeMoves := []string{}
-	for move, isSafe := range possibleMoves {
-		if isSafe {
-			safeMoves = append(safeMoves, move)
+	if len(valid_moves) == 0 {
+		return BattlesnakeMoveResponse{
+			Move: rules.MoveUp,
 		}
 	}
 
-	if len(safeMoves) == 0 {
-		nextMove = "down"
-		log.Printf("%s MOVE %d: No safe moves detected! Moving %s\n", state.Game.ID, state.Turn, nextMove)
-	} else {
-		nextMove = safeMoves[rand.Intn(len(safeMoves))]
-		log.Printf("%s MOVE %d: %s\n", state.Game.ID, state.Turn, nextMove)
-	}
 	return BattlesnakeMoveResponse{
-		Move: nextMove,
+		Move: valid_moves[0].Move,
 	}
 }

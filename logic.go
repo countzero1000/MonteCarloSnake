65ab45789c5e54b5ff6ee92cf05709bfa763df22
Edward Garmon <garmonedward@gmail.com>
2022-07-17 18:40:44 -0500

    change back to monte move

--- logic.go
+++ logic.go
@@ -46,6 +46,6 @@ func move(state GameState) BattlesnakeMoveResponse {
 	tree := new_tree(state)
 
 	return BattlesnakeMoveResponse{
-		Move: tree.find_food_move(),
+		Move: tree.monte_move().ID,
 	}
 }
